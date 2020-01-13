9edf83b88c08ea48a764f5329f77d7330188cdc6
Hunter <1737087925@qq.com>
2020-01-13 19:47:38 +0800

    :sparkles: (src/components/render-comp.vue) add src/components/render-comp.vue for test render comp…

---
+++ src/components/render-comp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+import { h } from "@/learn-vue/vdom/h";
+import { render } from "@/learn-vue/vdom/render";
+import { query } from "@/learn-vue/vdom/constant";
+
+export default {
+  name: "RenderComp",
+  data() {
+    return {
+      id: "render-comp-container"
+    };
+  },
+  mounted() {
+    const container = query(`#${this.id}`);
+
+    render(this.getNormalCompVnode(), container);
+    render(this.getFunctionalCompVnode(), container);
+  },
+  methods: {
+    getNormalCompVnode() {
+      class ClassComp {
+        render() {
+          return h("span");
+        }
+      }
+
+      return h(ClassComp);
+    },
+    getFunctionalCompVnode() {
+      const FuncitonalComp = () => h("p");
+      FuncitonalComp.functional = true;
+      return h(FuncitonalComp);
+    }
+  }
+};
+</script>
