a3f2d6809345470f4cc99dc224cdad9800623637
Hunter <1737087925@qq.com>
2020-01-13 10:50:03 +0800

    :sparkles: (src/components/render-html-element.vue, src/routes.js) 添加用于测试 render() 函数挂载 h…

---
+++ src/components/render-html-element.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+import { h } from "@/learn-vue/vdom/h";
+import { render } from "@/learn-vue/vdom/render";
+import { query } from "@/learn-vue/vdom/constant";
+
+export default {
+  name: "RenderHTMLElement",
+  data() {
+    return {
+      id: "render-html-element-container"
+    };
+  },
+  mounted() {
+    const container = query(`#${this.id}`);
+    const htmlElmVnode = h("span");
+    const svgVnode = h("svg");
+
+    render(htmlElmVnode, container);
+    render(svgVnode, container);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import Home from "@/components/home";
+import RenderHTMLElement from "@/components/render-html-element.vue";
+
+export const RENDER_HTML_ELEMENT = "/test/render-html-element";
 
 export default [
   {
     path: "",
     component: Home
+  },
+  {
+    path: RENDER_HTML_ELEMENT,
+    component: RenderHTMLElement
   }
 ];
