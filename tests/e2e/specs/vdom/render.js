0bf56128580ca22fd21b4b256ac2dee20f19f4bf
Hunter <1737087925@qq.com>
2020-01-13 19:48:06 +0800

    :white_check_mark: (tests/e2e/specs/vdom/render.js) add src/components/render-comp.vue e2e test

--- tests/e2e/specs/vdom/render.js
+++ tests/e2e/specs/vdom/render.js
@@ -2,13 +2,25 @@ module.exports = {
   "test render html element"(browser) {
     browser
       .url("http://localhost:8000/test/render-html-element")
-      .waitForElementVisible("span", 3000)
+      .waitForElementVisible("span", 1000)
       .end();
   },
   "test render svg element"(browser) {
     browser
       .url("http://localhost:8080/test/render-html-element")
-      .waitForElementVisible("svg", 3000)
+      .waitForElementVisible("svg", 1000)
+      .end();
+  },
+  "test render stateful component"(browser) {
+    browser
+      .url("http://localhost:8080/test/render-comp")
+      .waitForElementVisible("span", 1000)
+      .end();
+  },
+  "test render functional component"(browser) {
+    browser
+      .url("http://localhost:8080/test/render-comp")
+      .waitForElementVisible("p", 1000)
       .end();
   }
 };
