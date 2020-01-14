3ecf4a6a59dca4146f7833c76f1f82592323ad92
Hunter <1737087925@qq.com>
2020-01-15 00:34:14 +0800

    :sparkles: (src/components/render-fragment.vue) add src/components/render-fragment.vue as mount fra…

---
+++ src/components/render-fragment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+import { h } from "@/learn-vue/vdom/h";
+import { render } from "@/learn-vue/vdom/render";
+import { query, FRAGMENT } from "@/learn-vue/vdom/constant";
+
+export default {
+  name: "RenderFragment",
+  data() {
+    return {
+      id: "render-fragment-container"
+    };
+  },
+  mounted() {
+    const container = query(`#${this.id}`);
+    const fragmentVnode = h(FRAGMENT, null, [h("span"), h("p")]);
+    render(fragmentVnode, container);
+  }
+};
+</script>
