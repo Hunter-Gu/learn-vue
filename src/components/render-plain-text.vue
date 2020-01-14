b90318e5be459525a4b185de82cb682ffabe0811
Hunter <1737087925@qq.com>
2020-01-15 00:33:49 +0800

    :sparkles: (src/components/render-plain-text.vue) add src/components/render-plain-text.vue as mount…

---
+++ src/components/render-plain-text.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script>
+import { h } from "@/learn-vue/vdom/h";
+import { render } from "@/learn-vue/vdom/render";
+import { query } from "@/learn-vue/vdom/constant";
+
+export default {
+  name: "RenderPlainText",
+  data() {
+    return {
+      id: "render-plain-text-container"
+    };
+  },
+  mounted() {
+    const container = query(`#${this.id}`);
+    const plainTextVnode = h(null, null, "1234");
+
+    render(plainTextVnode, container);
+  }
+};
+</script>
