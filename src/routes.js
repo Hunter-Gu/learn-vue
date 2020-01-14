6a07adc2ed9d4f0a09fc40298f3121930582c884
Hunter <1737087925@qq.com>
2020-01-15 00:35:42 +0800

    :sparkles: (src/routes.js) add examples router path

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Home from "@/components/home";
-import RenderHTMLElement from "@/components/render-html-element.vue";
+import RenderPlainText from "@/components/render-plain-text";
+import RenderHTMLElement from "@/components/render-html-element";
 import RenderComp from "@/components/render-comp";
+import RenderFragment from "@/components/render-fragment";
+import RenderPortal from "@/components/render-portal";
 
+export const RENDER_PLAIN_TEXT = "/text/render-plain-text";
 export const RENDER_HTML_ELEMENT = "/test/render-html-element";
 export const RENDER_COMP = "/test/render-comp";
+export const REDNER_FRAGMENT = "/test/render-fragment";
+export const REDNER_PORTAL = "/test/render-portal";
 
 export default [
   {
@@ -17,5 +23,17 @@ export default [
   {
     path: RENDER_COMP,
     component: RenderComp
+  },
+  {
+    path: RENDER_PLAIN_TEXT,
+    component: RenderPlainText
+  },
+  {
+    path: REDNER_FRAGMENT,
+    component: RenderFragment
+  },
+  {
+    path: REDNER_PORTAL,
+    component: RenderPortal
   }
 ];
