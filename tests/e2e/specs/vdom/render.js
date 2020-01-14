33ef3f9e106ac16cada9153166ea869e254587c8
Hunter <1737087925@qq.com>
2020-01-15 00:36:17 +0800

    :white_check_mark: (tests/e2e/specs/vdom/render.js) complete render e2e test

--- tests/e2e/specs/vdom/render.js
+++ tests/e2e/specs/vdom/render.js
@@ -1,7 +1,13 @@
 module.exports = {
+  "text render plain text"(browser) {
+    browser
+      .url("http://localhost:8080/text/render-plain-text")
+      .assert.containsText("#render-plain-text-container", "123")
+      .end();
+  },
   "test render html element"(browser) {
     browser
-      .url("http://localhost:8000/test/render-html-element")
+      .url("http://localhost:8080/test/render-html-element")
       .waitForElementVisible("span", 1000)
       .end();
   },
@@ -21,6 +27,19 @@ module.exports = {
     browser
       .url("http://localhost:8080/test/render-comp")
       .waitForElementVisible("p", 1000)
+      .end();
+  },
+  "test render fragment"(browser) {
+    browser
+      .url("http://localhost:8080/test/render-fragment")
+      .waitForElementVisible("p", 1000)
+      .waitForElementVisible("span", 1000)
+      .end();
+  },
+  "test render portal"(browser) {
+    browser
+      .url("http://localhost:8080/test/render-portal")
+      .waitForElementVisible("span", 1000)
       .end();
   }
 };
