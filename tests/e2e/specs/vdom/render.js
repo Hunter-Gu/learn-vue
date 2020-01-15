cceef4b677f58a22cdad397fc6c44b627e26eb8d
Hunter <1737087925@qq.com>
2020-01-16 00:28:47 +0800

    :recycle: (tests/e2e/specs/vdom/render.js) refactor code

--- tests/e2e/specs/vdom/render.js
+++ tests/e2e/specs/vdom/render.js
@@ -1,44 +1,44 @@
 module.exports = {
-  "text render plain text"(browser) {
+  "test render plain text"(browser) {
     browser
-      .url("http://localhost:8080/text/render-plain-text")
+      .url("http://localhost:8080/example/render-plain-text")
       .assert.containsText("#render-plain-text-container", "123")
       .end();
   },
   "test render html element"(browser) {
     browser
-      .url("http://localhost:8080/test/render-html-element")
+      .url("http://localhost:8080/example/render-html-element")
       .waitForElementVisible("span", 1000)
       .end();
   },
   "test render svg element"(browser) {
     browser
-      .url("http://localhost:8080/test/render-html-element")
+      .url("http://localhost:8080/example/render-html-element")
       .waitForElementVisible("svg", 1000)
       .end();
   },
   "test render stateful component"(browser) {
     browser
-      .url("http://localhost:8080/test/render-comp")
+      .url("http://localhost:8080/example/render-comp")
       .waitForElementVisible("span", 1000)
       .end();
   },
   "test render functional component"(browser) {
     browser
-      .url("http://localhost:8080/test/render-comp")
+      .url("http://localhost:8080/example/render-comp")
       .waitForElementVisible("p", 1000)
       .end();
   },
   "test render fragment"(browser) {
     browser
-      .url("http://localhost:8080/test/render-fragment")
+      .url("http://localhost:8080/example/render-fragment")
       .waitForElementVisible("p", 1000)
       .waitForElementVisible("span", 1000)
       .end();
   },
   "test render portal"(browser) {
     browser
-      .url("http://localhost:8080/test/render-portal")
+      .url("http://localhost:8080/example/render-portal")
       .waitForElementVisible("span", 1000)
       .end();
   }
