0b72dcfb040331799e2024a4e35fea7bac0b7aaa
Hunter <1737087925@qq.com>
2020-01-20 01:40:20 +0800

    :sparkles: (src/components/vdom/patch/) add patch text example

---
+++ src/components/vdom/patch/patch-text.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :id="this.id"></div>
+</template>
+
+<script>
+import { query } from "@/learn-vue/vdom/constant";
+import { h } from "../../../learn-vue/vdom/h";
+import { render } from "../../../learn-vue/vdom/render";
+
+export default {
+  name: "PatchText",
+  data() {
+    return {
+      id: "patch-text-container"
+    };
+  },
+  mounted() {
+    const text = "hello world";
+    const container = query(`#${this.id}`);
+    const vnode = h(
+      "div",
+      {
+        class: this.id
+      },
+      text
+    );
+    render(vnode, container);
+
+    setTimeout(() => {
+      const newVnode = h(
+        "div",
+        {
+          class: this.id
+        },
+        text.toUpperCase()
+      );
+      render(newVnode, container);
+    }, 3000);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/routes/patch.route.js
+++ src/routes/patch.route.js
@@ -1,3 +1,8 @@
-export default [
+import PatchText from "@/components/vdom/patch/patch-text";
 
+export default [
+  {
+    path: "patch-text",
+    component: PatchText
+  }
 ];
