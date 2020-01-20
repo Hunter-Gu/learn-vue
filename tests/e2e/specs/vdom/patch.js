4ff3babc9eb749aef89f9ab2464fcd1a60c9d803
Hunter <1737087925@qq.com>
2020-01-20 12:43:34 +0800

    :white_check_mark: (tests/e2e/specs/vdom/patch.js) add e2e test for patch-html-element

--- tests/e2e/specs/vdom/patch.js
+++ tests/e2e/specs/vdom/patch.js
@@ -11,5 +11,13 @@ module.exports = {
       .pause(3000, () => {
         browser.assert.containsText(className, text.toUpperCase()).end();
       });
+  },
+  "test patch html element"(browser) {
+    browser
+      .url(BASE_URL + "patch-html-element")
+      .waitForElementVisible("p", 1000)
+      .pause(3000, () => {
+        browser.waitForElementVisible("a", 1000).end();
+      });
   }
 };
