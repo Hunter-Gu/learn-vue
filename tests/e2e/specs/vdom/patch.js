1f710a8adcf5d4fd6baab809efceb7241b85bb20
Hunter <1737087925@qq.com>
2020-01-20 12:31:04 +0800

    :white_check_mark: (tests/e2e/specs/vdom/patch.js) add e2e test for patch-text example

--- tests/e2e/specs/vdom/patch.js
+++ tests/e2e/specs/vdom/patch.js
@@ -3,13 +3,13 @@ const BASE_URL = "http://localhost:8080/example/patch/";
 module.exports = {
   "test patch plain text"(browser) {
     const className = ".patch-text-container";
-    // const text = "hello world";
+    const text = "hello world";
     browser
       .url(BASE_URL + "patch-text")
       .waitForElementVisible(className, 1000)
-      // .assert.containsText(className, text)
-      // .after(3000)
-      // .assert.containsText(className, text.toUpperCase())
-      .end();
+      .assert.containsText(className, text)
+      .pause(3000, () => {
+        browser.assert.containsText(className, text.toUpperCase()).end();
+      });
   }
 };
