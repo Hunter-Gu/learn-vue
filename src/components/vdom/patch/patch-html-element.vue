73974676c5446a958be36f3bb4728c1257cc2b09
Hunter <1737087925@qq.com>
2020-01-20 12:43:16 +0800

    :sparkles: (src/components/vdom/patch/patch-html-element.vue) add example for patch-html-element

--- src/components/vdom/patch/patch-html-element.vue
+++ src/components/vdom/patch/patch-html-element.vue
@@ -8,32 +8,19 @@ import { h } from "../../../learn-vue/vdom/h";
 import { render } from "../../../learn-vue/vdom/render";
 
 export default {
-  name: "PatchText",
+  name: "PatchHtmlElement",
   data() {
     return {
-      id: "patch-text-container"
+      id: "patch-html-element-container"
     };
   },
   mounted() {
-    const text = "hello world";
     const container = query(`#${this.id}`);
-    const vnode = h(
-      "div",
-      {
-        class: this.id
-      },
-      text
-    );
+    const vnode = h("p");
     render(vnode, container);
 
     setTimeout(() => {
-      const newVnode = h(
-        "div",
-        {
-          class: this.id
-        },
-        text.toUpperCase()
-      );
+      const newVnode = h("a");
       render(newVnode, container);
     }, 3000);
   }
--- src/routes/patch.route.js
+++ src/routes/patch.route.js
@@ -1,8 +1,13 @@
 import PatchText from "@/components/vdom/patch/patch-text";
+import PatchHtmlElement from "@/components/vdom/patch/patch-html-element";
 
 export default [
   {
     path: "patch-text",
     component: PatchText
+  },
+  {
+    path: "patch-html-element",
+    component: PatchHtmlElement
   }
 ];
