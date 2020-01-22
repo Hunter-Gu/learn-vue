1002718fb11dd238e360ba57d4b86eaf08ac30b4
Hunter <1737087925@qq.com>
2020-01-22 12:20:38 +0800

    :white_check_mark: (tests/e2e/specs/vdom/patch.js) add e2e test for patch children

--- tests/e2e/specs/vdom/patch.js
+++ tests/e2e/specs/vdom/patch.js
@@ -19,5 +19,21 @@ module.exports = {
       .pause(3000, () => {
         browser.waitForElementVisible("a", 1000).end();
       });
+  },
+  "test patch children"(browser) {
+    browser
+      .url(BASE_URL + "patch-children")
+      .waitForElementVisible("#patch-children-container", 1000)
+      .element("css selector", `div > p`, result => {
+        browser.assert.equal(result.status, 0);
+      })
+      .pause(3000, () => {
+        browser
+          .waitForElementNotPresent("p", 1000)
+          .element("css selector", `div > span`, result => {
+            browser.assert.equal(result.status, 0);
+          })
+          .end();
+      });
   }
 };
