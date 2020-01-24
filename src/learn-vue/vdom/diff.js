27ef1fd83501869948a4a4cd6cc0a30b6f21d0b9
Hunter <1737087925@qq.com>
2020-01-24 14:30:13 +0800

    :sparkles: (src/learn-vue/vdom/diff.js) optimize diff by lastIndex algorithm

--- src/learn-vue/vdom/diff.js
+++ src/learn-vue/vdom/diff.js
@@ -1,17 +1,33 @@
 import { patch } from "./patch";
 
 /**
- * @description 最佳的方式是通过移动节点来达到目的， 因为这样的场景更多
- *              关键是在新、 旧节点中保存映射关系， 以便找到可复用节点 ---> key
- * @warn 双层 for 循环 T(n) = O(n^2)
+ * @description 寻找最大索引， 该方式以 children 中的第一个节点为基准
  */
 export function diff(children, prevChildren, container) {
-  for (let i = 0; i < children; i++) {
+  // 最大索引
+  let lastIndex = 0;
+  for (let i = 0; i < children.length; i++) {
     const vnode = children[i];
-    for (let j = 0; j < prevChildren; j++) {
+    for (let j = 0; j < prevChildren.length; j++) {
       const prevVnode = prevChildren[j];
       if (vnode.key === prevVnode.key) {
         patch(vnode, prevVnode, container);
+        if (j < lastIndex) {
+          // 节点被移动了
+          // prev       a b c
+          // next           c b a
+          // idx            2 1 0
+          // lastIndex      2 2 2
+
+          // 1.children 中的第一个节点不处理
+          // 2.之后的节点， 插入到第一个节点的后面
+          // 为什么需要 nextSibling？
+          // 因为新节点需要插到第一个节点的后面， 也就是第一个节点的下一个兄弟节点的前面
+          const refNode = children[i - 1].$el.nextSibling;
+          container.insertBefore(prevVnode.$el, refNode);
+        } else {
+          lastIndex = j;
+        }
       }
     }
   }
